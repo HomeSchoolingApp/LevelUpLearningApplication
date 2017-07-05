8465b3f8a6aa3551e712711e773bfd0c19caa12c
Daniel De La Fuentes <d33fu3nt35@gmail.com>
2017-07-05 11:39:21 -0500

    Initial commit & push for new repo.
    Added updated changes to .gitignore, as well as added index view.

---
+++ src/main/java/com/leveluplearning/LeveluplearningApplication.java
@@ -0,0 +1,12 @@
+package com.leveluplearning;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LeveluplearningApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LeveluplearningApplication.class, args);
+	}
+}
---
+++ src/test/java/com/leveluplearning/LeveluplearningApplicationTests.java
@@ -0,0 +1,16 @@
+package com.leveluplearning;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class LeveluplearningApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
