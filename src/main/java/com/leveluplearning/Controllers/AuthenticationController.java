ce3941ebd4cb1aea313f7970681e6339fb587c91
Daniel De La Fuentes <d33fu3nt35@gmail.com>
2017-07-06 14:23:11 -0500

    Added Footer to INDEX & created AuthenticationController & usersController

---
+++ src/main/java/com/leveluplearning/Controllers/AuthenticationController.java
@@ -0,0 +1,23 @@
+//package com.leveluplearning.Controllers;
+//
+//import org.springframework.stereotype.Controller;
+//import org.springframework.ui.Model;
+//import org.springframework.web.bind.annotation.GetMapping;
+//
+///**
+// * Created by daniel on 7/6/17.
+// */
+//
+//@Controller
+//public class AuthenticationController {
+//    @GetMapping("/login")
+//    public String showLoginForm() {
+//        return "login";
+//    }
+//
+//    @GetMapping("/register")
+//    public String showRegisterForm(Model model) {
+////        model.addAttribute("user", new User());
+//        return "register";
+//    }
+//}
