78111fc76687dcc52edbb38489a52904712ab602
rcortezk9 <rcortezk9@gmail.com>
2017-07-06 13:06:28 -0500

    Created Messenger model

---
+++ src/main/java/com/leveluplearning/models/Messanger.java
@@ -0,0 +1,77 @@
+package com.leveluplearning.models;
+
+import javax.persistence.*;
+import javax.xml.soap.Text;
+import java.util.Date;
+
+/**
+ * Created by renecortez on 7/6/17.
+ */
+
+@Entity
+@Table(name = "messenger")
+public class Messanger {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;//would this be idMessenger
+
+    @Column(name = "sender")
+    private int sender;
+
+    @Column(name = "reciever")
+    private int reciever;
+
+    @Column(name = "message")
+    private Text message;
+
+    @Column(name = "date_sent")
+    private Date dateSent;
+
+    public Messanger(int sender, int reciever, Text message, Date dateSent) {
+        this.sender = sender;
+        this.reciever = reciever;
+        this.message = message;
+        this.dateSent = dateSent;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public int getSender() {
+        return sender;
+    }
+
+    public void setSender(int sender) {
+        this.sender = sender;
+    }
+
+    public int getReciever() {
+        return reciever;
+    }
+
+    public void setReciever(int reciever) {
+        this.reciever = reciever;
+    }
+
+    public Text getMessage() {
+        return message;
+    }
+
+    public void setMessage(Text message) {
+        this.message = message;
+    }
+
+    public Date getDateSent() {
+        return dateSent;
+    }
+
+    public void setDateSent(Date dateSent) {
+        this.dateSent = dateSent;
+    }
+}
