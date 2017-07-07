a0089b6a861c9659b98679ba7328cf6bee1271a5
Daniel De La Fuentes <d33fu3nt35@gmail.com>
2017-07-07 13:57:26 -0500

    Fixed Thymeleaf Arraylist object issues

--- src/main/java/com/leveluplearning/Controllers/UsersController.java
+++ src/main/java/com/leveluplearning/Controllers/UsersController.java
@@ -7,6 +7,7 @@ import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 /**
  * Created by daniel on 7/6/17.
@@ -22,11 +23,16 @@ public class UsersController {
     private PasswordEncoder passwordEncoder;
 
     @PostMapping("/users/register")
-    public String saveUser(@ModelAttribute User user) {
+    public String saveUser(@ModelAttribute User user,
+                           @RequestParam (name = "role") String role) {
+
+        System.out.println(role);
 
         user.setPassword(passwordEncoder.encode(user.getPassword()));
 
         usersDao.save(user);
+
+
 
 
 
