87e01a1ae377914a6cd5a5159dc802e55206aa7f
rcortezk9 <rcortezk9@gmail.com>
2017-07-07 11:08:10 -0500

    added a line

--- src/main/java/com/leveluplearning/repositories/UsersRepo.java
+++ src/main/java/com/leveluplearning/repositories/UsersRepo.java
@@ -7,6 +7,7 @@ import org.springframework.stereotype.Repository;
 /**
  * Created by daniel on 7/6/17.
  */
+
 @Repository
 public interface UsersRepo extends CrudRepository<User, Long> {
     public User findByUsername (String username);
