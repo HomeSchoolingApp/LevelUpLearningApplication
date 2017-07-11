ba22ee87f0c41a2e09c84c9eb19ba69a749ee245
Daniel De La Fuentes <d33fu3nt35@gmail.com>
2017-07-10 23:18:51 -0500

    Added validation API that way forms will need to be validated even through mobile view.

---
+++ src/main/resources/static/js/modernizr-custom.js
@@ -0,0 +1,24 @@
+/**
+ * Created by daniel on 7/10/17.
+ */
+
+(function () {
+    webshim.setOptions('forms', {
+        lazyCustomMessages: true,
+        iVal: {
+            sel: '.ws-validate',
+            handleBubble: 'hide', // hide error bubble
+
+            //add bootstrap specific classes
+            errorMessageClass: 'help-block',
+            successWrapperClass: 'has-success',
+            errorWrapperClass: 'has-error',
+
+            //add config to find right wrapper
+            fieldWrapper: '.form-group'
+        }
+    });
+
+    //load forms polyfill + iVal feature
+    webshim.polyfill('forms');
+})();
