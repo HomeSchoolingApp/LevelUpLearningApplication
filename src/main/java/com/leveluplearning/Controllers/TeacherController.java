2932ce614b63eee2e8f3ddc09312bbbf5fb29f29
rcortezk9 <rcortezk9@gmail.com>
2017-07-12 17:38:11 -0500

    Added styling for search bar area

--- src/main/java/com/leveluplearning/Controllers/TeacherController.java
+++ src/main/java/com/leveluplearning/Controllers/TeacherController.java
@@ -2,7 +2,9 @@ package com.leveluplearning.Controllers;
 
 import com.leveluplearning.models.User;
 import com.leveluplearning.repositories.TeacherRepo;
+import com.leveluplearning.repositories.UsersRepo;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -21,9 +23,14 @@ public class TeacherController {
     @Autowired
     TeacherRepo teacherDao;
 
+    @Autowired
+    UsersRepo usersDao;
+
     @GetMapping("/teachers")
     public String viewAll(Model model) {
         Iterable<User> users = teacherDao.findAllTeachers();
+        User user = (User) SecurityContextHolder.getContext().getAuthentication().getPrincipal();
+        model.addAttribute("user", user);
         model.addAttribute("teachers", users);
         return "views/viewAllTeacherProfiles";
     }
@@ -32,6 +39,7 @@ public class TeacherController {
     public String searchTeacher(@RequestParam(name = "searchTerm") String searchTerm, Model model){
         List<User> teachers = teacherDao.findTeachersByName("%" + searchTerm + "%");
         model.addAttribute("teachers", teachers);
+
         return "views/viewAllTeacherProfiles";
     }
 
